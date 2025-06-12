32e120582c4bd07baf04eb7c34d2b542fba2a755
anoj-kt <anojk28@gmail.com>
2025-06-12 10:20:51 +0200

    Added comments

--- module-02-web-apis/03-fetch-api/index.js
+++ module-02-web-apis/03-fetch-api/index.js
@@ -1,3 +1,4 @@
+//Constructing your own promise. You rarely construct your own promise so ignore this for now if its confusing.
 const groceryPromise = new Promise((resolve, reject) => {
   reject('Failed');
 });
@@ -10,6 +11,8 @@ groceryPromise
     // console.log(rejectedPromise);
   });
 
+//Function to create new elements
+//We will call this with some actual data from an external API
 const createUserList = (name) => {
   const userItem = document.createElement('li');
   userItem.textContent = name;
@@ -19,23 +22,27 @@ const createUserList = (name) => {
 //FETCH API
 fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => {
-    return response.json();
+    //We need to convert the json to actual JS.
+    return response.json(); //.json() returns a promise requiring a second .then()
   })
   .then((data) => {
+    //Once the json to JS conversion is successful, the data is passed to this callback function.
+    //We can then do whatever we want with the data. Eg: DOM Manipulation
     const whatUserChose = 'name';
     data.forEach((user) => createUserList(user[whatUserChose]));
   })
   .catch((error) => {
+    //Any errors that happen in the fetch() function or in the .then() functions will be passed here
     console.log(error);
   });
 
+//POST
 const newPost = {
   title: 'My new post',
   body: 'This is the actual content of the post',
   userId: 5,
 };
 
-//POST
 fetch('https://jsonplaceholder.typicode.com/posts', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
